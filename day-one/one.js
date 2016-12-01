6c198e7c68ab17f445995271627a1df87208085a
Raquel Moss <raquelxmoss@gmail.com>
2016-12-01 22:27:44 +1300

    add day one part one solution

---
+++ day-one/one.js
@@ -0,0 +1,25 @@
+function calculateVector (dir, {x, y}, blocks) {
+  return {
+    0: {x, y: y + blocks},
+    1: {x: x + blocks, y},
+    2: {x, y: y - blocks},
+    3: {x: x - blocks, y}
+  }[dir];
+}
+
+function distanceAsVector (instructions) {
+  return instructions.reduce((acc, curr) => {
+    const dir = Math.abs((curr[0] === 'L' ? -1 : 1) + acc.dir) % 4;
+    const blocks = parseInt(curr.slice(1, curr.length));
+
+    const newVector = calculateVector(dir, {x: acc.x, y: acc.y}, blocks);
+
+    return Object.assign(newVector, {dir});
+  }, { x: 0, y: 0, dir: 0 });
+}
+
+export function howFarTo (input) {
+  const instructions = distanceAsVector(input.split(', '));
+
+  return Math.abs(instructions.x + instructions.y);
+}
---
+++ test/day-one-test.js
@@ -0,0 +1,18 @@
+/* globals it, describe */
+
+import assert from 'assert';
+import { howFarTo } from '../day-one/one';
+
+describe('howFarTo', () => {
+  it('tells us how far we are from the destination', () => {
+    const input1 = 'R2, L3';
+    const input2 = 'R2, R2, R2';
+    const input3 = 'R5, L5, R5, R3';
+    const input4 = 'L5, R1, L5, L1, R5, R1, R1, L4, L1, L3, R2, R4, L4, L1, L1, R2, R4, R3, L1, R4, L4, L5, L4, R4, L5, R1, R5, L2, R1, R3, L2, L4, L4, R1, L19, R5, R1, R4, L5, L4, R5, L1, L1, R48, R5, R5, L2, R4, R4, R1, R3, L1, L4, L5, R1, L4, L2, L5, R5, L2, R74, R4, L1, R18, R5, L4, L2, R5, R2, L4, R4, R3, R3, R2, R1, L3, L2, L5, L5, L2, L1, R1, R5, R4, L3, R5, L1, L3, R4, L1, L3, L2, R1, R3, R2, R5, L3, L1, L1, R5, L4, L5, R5, R2, L5, R2, L1, L5, L3, L5, L5, L1, R1, L4, L3, L1, R2, R5, L1, L3, R4, R5, L4, L1, R5, L1, R5, R5, R5, R2, R1, R2, L5, L5, L5, R4, L5, L4, L4, R5, L2, R1, R5, L1, L5, R4, L3, R4, L2, R3, R3, R3, L2, L2, L2, L1, L4, R3, L4, L2, R2, R5, L1, R2';
+
+    assert.equal(howFarTo(input1), 5);
+    assert.equal(howFarTo(input2), 2);
+    assert.equal(howFarTo(input3), 12);
+    assert.equal(howFarTo(input4), 171);
+  });
+});
