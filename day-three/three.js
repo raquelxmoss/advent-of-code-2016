401d747b469b11e7325465a0716d882838bd5c4a
Raquel Moss <raquelxmoss@gmail.com>
2016-12-04 15:40:24 +1300

    day 3 challenge 2

--- day-three/three.js
+++ day-three/three.js
@@ -1,21 +1,35 @@
-function parseSides (input) {
-  return input
-    .split(' ')
-    .filter(c => c !== '')
-    .map(side => side.trim())
-    .map(len => parseInt(len))
-    .sort((a, b) => a - b);
+import { transpose } from 'mathjs';
+import _ from 'lodash';
+
+function parseInput (input) {
+  const parsed = input
+    .split('\n')
+    .map(str => str.trim())
+    .filter(row => row !== '')
+    .map(row => row.split(' '))
+    .map(row => row.filter(c => c !== ''))
+    .map(row => row.map(i => parseInt(i)));
+
+  const transposed = transpose(parsed);
+  const chunked = transposed.map(row => _.chunk(row, 3));
+
+  const result = _.flatten(chunked);
+
+  return result;
+}
+
+function sortSides (input) {
+  return input.sort((a, b) => a - b);
 }
 
 function validTriangle (sides) {
-  const lengths = parseSides(sides);
+  const sortedSides = sortSides(sides);
 
-  return lengths[0] + lengths[1] > lengths[2];
+  return sortedSides[0] + sortedSides[1] > sortedSides[2];
 }
 
 export default function validTriangleCount (input) {
-  return input
-    .split('\n')
+  return parseInput(input)
     .map(triangle => validTriangle(triangle))
     .filter(result => result === true)
     .length;
--- test/day-three-test.js
+++ test/day-three-test.js
@@ -5,11 +5,20 @@ import { input } from '../day-three/input';
 
 describe('validTriangleCount', () => {
   it('tells us how many valid triangles there are', () => {
-    const exampleInput = `3, 3, 3`;
-    const exampleInput2 = `2, 2, 8`
+    const exampleInput = `
+      3 0 0
+      3 0 3
+      3 0 0
+    `;
+
+    const exampleInput2 = `
+      810  679    9
+      783  255  616
+      545  626  626
+    `;
 
     assert.equal(validTriangleCount(exampleInput), 1);
-    assert.equal(validTriangleCount(exampleInput2), 0);
-    assert.equal(validTriangleCount(input), 869);
+    assert.equal(validTriangleCount(exampleInput2), 2);
+    assert.equal(validTriangleCount(input), 1544);
   });
 });
